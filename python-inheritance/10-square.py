4f227b55d537b206ba4ccbf9bf86999be38bb955
Islam <islam@example.com>
2025-12-14 22:08:51 +0400

    All

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,14 @@
-class Square(__import__('9-rectangle').Rectangle):
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle"""
 
-    def area(self):
-        return self._Rectangle__width * self._Rectangle__height
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a Square with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,10 +1,22 @@
-class Square(__import__('9-rectangle').Rectangle):
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle"""
+
     def __init__(self, size):
+        """Initialize a Square with size"""
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        return self._Rectangle__width * self._Rectangle__height
+        """Calculate the area of the square"""
+        return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        """Return the string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
